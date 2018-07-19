e60ffa579d99070993e6fda1cd01c2189387c0c9
David McElroy <david.mcelroy@scotland.pnn.police.uk>
2018-07-19 16:00:30 +0100

    add mock match data

---
+++ src/mocks/data-matches.js
@@ -0,0 +1,54 @@
+const matches = [];
+
+matches.push(
+  {
+    id: 'f0fe2736-69a4-46b3-a8a7-5b2c48a34c5c',
+    apiId: 12345,
+    date: '2018-08-10T19:00:00Z',
+    matchday: 1,
+    stage: 'REGULAR_SEASON',
+    group: 'Regular Season',
+    lastUpdated: '2018-07-07T16:01:35Z',
+    homeTeam: {
+      name: 'Team 1',
+    },
+    awayTeam: {
+      name: 'Team 2',
+    },
+    season: {},
+    winner: null,
+    duration: null,
+    fullTimeHome: null,
+    fullTimeAway: null,
+    halfTimeHome: null,
+    halfTimeAway: null,
+    extraTimeHome: null,
+    extraTimeAway: null,
+    penaltiesHome: null,
+    penaltiesAway: null,
+  },
+  {
+    id: '49a529eb-b562-44af-8ff0-dbe17b221cce',
+    apiId: 12346,
+    date: '2018-08-10T19:00:00Z',
+    matchday: 1,
+    stage: 'REGULAR_SEASON',
+    group: 'Regular Season',
+    lastUpdated: '2018-07-07T16:01:35Z',
+    homeTeam: { name: 'Team 2' },
+    awayTeam: { name: 'Team 1' },
+    season: {},
+    winner: null,
+    duration: null,
+    fullTimeHome: null,
+    fullTimeAway: null,
+    halfTimeHome: null,
+    halfTimeAway: null,
+    extraTimeHome: null,
+    extraTimeAway: null,
+    penaltiesHome: null,
+    penaltiesAway: null,
+  }
+);
+
+export default matches;
